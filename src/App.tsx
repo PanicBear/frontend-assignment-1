9b01bbe6a18ab86358bf1a07e1d67a1690dff796
Jongkeun Choi <jongkeun.ch@gmail.com>
2021-10-12 11:50:38 +0900

    Initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,111 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { uploadFile } from "./api";
+
+interface AttachedFile {
+  id: string;
+  name: string;
+  uploadProgress: number;
+}
+
+const initialFormState: {
+  name: string;
+  email: string;
+  files: AttachedFile[];
+} = {
+  name: "",
+  email: "",
+  files: [],
+};
 
 function App() {
+  const [formState, setFormState] = useState(() => initialFormState);
+
+  const handleChangeName: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    setFormState({ ...formState, name: e.target.value });
+  };
+  const handleChangeEmail: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    setFormState({ ...formState, email: e.target.value });
+  };
+
+  const handleChangeFile: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    if (e.target.files && e.target.files[0]) {
+      const fileObj = e.target.files[0];
+      const id = String(+new Date()) + Math.random();
+      const findIndexById = (id: string) =>
+        formState.files.findIndex((file) => file.id === id);
+      setFormState({
+        ...formState,
+        files: [
+          ...formState.files,
+          { id, name: fileObj.name, uploadProgress: 0 },
+        ],
+      });
+      uploadFile(e.target.files[0], (progress) => {
+        const idx = findIndexById(id);
+        const prev = formState.files.slice(0, idx);
+        const next = formState.files.slice(idx + 1);
+        const newState = {
+          ...formState,
+          files: [
+            ...prev,
+            { ...formState.files[idx], uploadProgress: progress },
+            ...next,
+          ],
+        };
+        setFormState(newState);
+      });
+    }
+    e.target.value = "";
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <h2>○○○ 기업 지원</h2>
+      <article
+        style={{
+          display: "flex",
+          gap: 16,
+          backgroundColor: "aliceblue",
+          width: 400,
+        }}
+      >
+        <form
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            gap: "8px",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <h3>지원서 작성</h3>
+          <input
+            type="text"
+            placeholder="이름"
+            style={{ width: 300 }}
+            value={formState.name}
+            onChange={handleChangeName}
+          />
+          <input
+            type="email"
+            placeholder="이메일"
+            style={{ width: 300 }}
+            value={formState.email}
+            onChange={handleChangeEmail}
+          />
+          <strong>이력서</strong>
+          <input
+            type="file"
+            style={{ width: 300 }}
+            onChange={handleChangeFile}
+          />
+          <ul>
+            {formState.files.map((file) => (
+              <li key={file.id}>
+                <em>{file.name}</em>
+              </li>
+            ))}
+          </ul>
+        </form>
+      </article>
     </div>
   );
 }
---
+++ src/api.ts
@@ -0,0 +1,17 @@
+export const uploadFile = async (
+  file: File,
+  progressFn: (progress: number) => void
+) => {
+  let progress = -1;
+  return new Promise((resolve) => {
+    const id = window.setInterval(() => {
+      progressFn(++progress);
+      if (progress >= 100) {
+        window.clearInterval(id);
+        resolve({
+          name: file.name,
+        });
+      }
+    }, 100);
+  });
+};
